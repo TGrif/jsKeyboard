e8915a088826da5cc5108830025bfd3da1ade292
TGrif <thomas.griffault@gmail.com>
2019-10-18 18:01:59 +0200

    Clean code

--- jsKeyboard.js
+++ jsKeyboard.js
@@ -1,8 +1,8 @@
 /**
  * jsKeyboard
- *  jsPlay Keyboard Display 
- *  
- * @author TGrif 2015 - MIT licence
+ *   Web Audio Keyboard
+ *
+ * TGrif 2015 - MIT licence
  */
 
 
@@ -10,204 +10,121 @@
 window.onload = function() {
 
 
+  var jsKeyboardContainer = d3.select("body")
+                              .append("svg")
+                              .attr("width", 1200)
+                              .attr("height", 200);
 
-        var            
-    
+  var whiteTouchlayer = jsKeyboardContainer.append('g');
+  var blackTouchlayer = jsKeyboardContainer.append('g');
 
-            jsKeyboard = {},
-            
-            
-            
-                svgContainer =
+  var blackTouchHeight = 96;
+  var blackTouchWidth = 25;
 
-                    d3.select("body")
+  var blackTouchShift = Math.floor(blackTouchWidth / 2);
 
-                        .append("svg")
-
-                            .attr("width", 1200)
-                            .attr("height", 800),
-
-
-        
-
-                whiteTouchlayer = svgContainer.append('g'),
-                blackTouchlayer = svgContainer.append('g'),
+  var whiteTouchHeight = 180;
+  var whiteTouchWidth = 40;
+  var whiteTouchWidthAlone = whiteTouchWidth + 1;
 
 
+  var octaves = octaves || [ /*1, 2,*/ 3, 4, 5, 6/*, 7, 8, 9, 10*/ ];  // 49 touch keyboard
 
+  var touch = touch || [ 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B' ];
 
-
-            blackTouchHeight = 96,
-            blackTouchWidth = 25,
-
-            blackTouchShift = Math.floor(blackTouchWidth / 2),
-
-
-
-            whiteTouchHeight = 180,
-            whiteTouchWidth = 40,
-            whiteTouchWidthAlone = whiteTouchWidth + 1,
+  var octaveShift = 0;
+  var nbOctaves = 4;
+  
+  var x = 40;
+  var j = 0;
         
         
         
-        
+  for (var o = 0; o < nbOctaves; o++) {
+  
+    if (o === nbOctaves - 1) {
+      touch.push('L');   // last white touch (whiteTouchWidthAlone)
+    }
+  
+  
+    for (var i = 0; i < touch.length; i++) {
 
-                octaves = octaves || [ /*1, 2,*/ 3, 4, 5, 6/*, 7, 8, 9, 10*/ ],  // 49 touch keyboard
-                
-                touch = touch || [ 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B' ],
+      if (touch[i].match(/^[A-Z]$/)) {    // match one char only
 
-
-
-        
-        octaveShift = 0,
-        
-        nbOctaves = 4,
-        
-        
-        x = 40,
-        
-        j = 0;
-        
-        
-        
-        
-        
-        for (var o = 0; o < nbOctaves; o++) {
-        
-        
-                if (o === nbOctaves - 1) {
-                    touch.push('L');   // last white touch (whiteTouchWidthAlone)
-                }
-        
-        
-        
-            for (var i = 0; i < touch.length; i++) {
-
-
-                if (touch[i].match(/^[A-Z]$/)) {    // match one char only
-
-
-
-                        if (i === touch.length - 1 && o === nbOctaves - 1) {
-                           whiteTouchWidth = whiteTouchWidthAlone;
-                        }
-
-
-
-
-                    whiteTouchlayer
-
-
-                            .append("rect")
-
-                                .attr("x",octaveShift + x * (i - j))
-                                .attr("y", 10)
-                                .attr("width", whiteTouchWidth)
-                                .attr("height", whiteTouchHeight)
-
-                                .attr("fill", "ivory")
-                                .attr("stroke-width", 1)
-                                .attr("stroke", "darkgray")
-
-                                .attr("id", touch[i] + octaves[o]);
-
-
-
-
-                } else {
-
-
-
-                    blackTouchlayer
-
-
-                            .append("rect")
-
-                                .attr("x",octaveShift + (x * (i - j)) - blackTouchShift)
-                                .attr("y", 10)
-                                .attr("width", blackTouchWidth)
-                                .attr("height", blackTouchHeight)
-
-                                .attr("fill", "ebony")
-                                .attr("stroke-width", 1)
-                                .attr("stroke", "darkgray")
-
-                                .attr("id", touch[i] + octaves[o]);
-
-
-                       j++;
-
-
-                }
-
-
-
-            }
-            
-
-
-                octaveShift += 480;
-
-
+        if (i === touch.length - 1 && o === nbOctaves - 1) {
+           whiteTouchWidth = whiteTouchWidthAlone;
         }
+
+        whiteTouchlayer.append("rect")
         
+          .attr("x",octaveShift + x * (i - j))
+          .attr("y", 10)
+          .attr("width", whiteTouchWidth)
+          .attr("height", whiteTouchHeight)
+
+          .attr("fill", "ivory")
+          .attr("stroke-width", 1)
+          .attr("stroke", "darkgray")
+
+          .attr("id", touch[i] + octaves[o]);
+
+
+      } else {
+
+        blackTouchlayer.append("rect")
+
+          .attr("x",octaveShift + (x * (i - j)) - blackTouchShift)
+          .attr("y", 10)
+          .attr("width", blackTouchWidth)
+          .attr("height", blackTouchHeight)
+
+          .attr("fill", "ebony")
+          .attr("stroke-width", 1)
+          .attr("stroke", "darkgray")
+
+          .attr("id", touch[i] + octaves[o]);
+
+           j++;
+
+      }
+
+    }
+
+    octaveShift += 480;
+
+  }
+
+
+  var that, touchColor;
+
+
+  d3.selectAll("rect")
+  
+    .on("mousedown", function() {
+      
+      that = d3.select(this);
+
+      touchColor = that.attr("fill");
+      that.attr("fill", "#fbc97f");   // light orange
+      
+      play(that.attr("id"));
+
+    })
+    
+    .on("mouseup", function() {
         
+      that.attr("fill", touchColor);
+      stop();
         
-        
-
-
-                var that, touchColor;
-
-
-
-
-
-
-            d3
-            
-                    .selectAll("rect")
-            
-            
-            
-            
-            
-                .on("mousedown", function() {
-                    
-                    that = d3.select(this);
-            
-                    touchColor = that.attr("fill");
-                    that.attr("fill", "#fbc97f");   // light orange
-                    
-                    
-                        play(that.attr("id"));
-                        
-
-                  })
-                  
-                  
-                  
-                  
-                .on("mouseup", function() {
-                    
-                    that.attr("fill", touchColor);
-                    stop();
-                    
-                })
-                
-                
-                
-        
-                .on("mousemove", function() {
-                    
-                    that = that || d3.select(this);
-                    touchColor = touchColor || that.attr("fill");
-                    that.attr("fill", touchColor);
-                    
-                });
-
+    })
     
     
-    
-    
-};
+    .on("mousemove", function() {
+      
+      that = that || d3.select(this);
+      touchColor = touchColor || that.attr("fill");
+      that.attr("fill", touchColor);
+        
+    });
 
+}
--- mod/sound.js
+++ mod/sound.js
@@ -1,75 +1,77 @@
 /**
  * jsKeyboard
- *  jsPlay Keyboard Play
+ *   Web Audio Keyboard
  *  
- * @author TGrif 2015 - MIT licence 
+ * TGrif 2015 - MIT licence 
  */
 
- 
- 
-var
 
-    audioContext = new AudioContext(),
-        
-        
-        osc,
-        
-         
-            frequency = {},
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+var osc;
+var volume = audioContext.createGain();
+volume.gain.value = 0.2;  // set default volume gain
+
+var oscType = ['sine', 'square', 'sawtooth', 'triangle'/*, 'custom'*/ ];
+var oscActualType = oscType[0];  // set default osc type
+
+var frequency = {};
+
+var octaves = [ 3, 4, 5, 6 ];  // TODO
+
+var touch = [ 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B' ];  // TODO
 
 
-                octaves = [ 3, 4, 5, 6 ],
-                
-                touch = [ 'C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B' ],
-               
-        
-
-    B2 = 123.471,
+var B2 = 123.471;
     
-    n = B2;     // TODO généraliser pour clavier 88 touches en partant du A4 = 440
+var n = B2;     // TODO généraliser pour clavier 88 touches en partant du A4 = 440
 
 
 
+for (var o = 0; o < octaves.length; o++) {
+  for (var i = 0; i < touch.length; i++) {
+    frequency[touch[i] + octaves[o]] = n = n * Math.pow(2, 1/12);
+  }
+}
 
+frequency['L6'] = n * Math.pow(2, 1/12);   // last key (C7)
+  
+//console.log(frequency);
+
+
+function play(touch) {
+
+  console.info(touch);
+  
+  if (frequency[touch]) { // if this is a touch
     
-    for (var o = 0; o < octaves.length; o++) {
-        for (var i = 0; i < touch.length; i++) {            
-            frequency[touch[i] + octaves[o]] = n = n * Math.pow(2, 1/12);
-        }
+    osc = audioContext.createOscillator();
+    
+    osc.frequency.value = frequency[touch];
+    osc.type = oscActualType;
+    
+    osc.connect(volume);
+    volume.connect(audioContext.destination);
+    
+    osc.start(0);
+    
+  } else { // this is a function button
+
+    switch (touch) {
+      case "touch1": // change osc type
+        let i = oscType.indexOf(oscActualType)
+        oscActualType = oscType[++i]
+        // d3.select("#touch1")    
+        //     .append("g")
+        //     .append("text").text('toto')
+        break;
     }
-    
-    frequency['L6'] = n * Math.pow(2, 1/12);   // last key (C7)
-    
-        //console.log(frequency);
-    
+    // console.log(touch)
+  }
+}
 
 
-
-
-
-    function play(touch) {
-        
-            console.info(touch);
-
-                
-        osc = audioContext.createOscillator();
-        
-        osc.frequency.value = frequency[touch];
-        osc.type = 'sine';
-        
-        
-        osc.connect(audioContext.destination);
-        osc.start(0);
-        
-    }
-    
-    
-    
-    
-    function stop() {
-        osc.stop(0);
-    }
-    
-    
-    
+function stop() {
+  if (osc) osc.stop(0);
+}
 
